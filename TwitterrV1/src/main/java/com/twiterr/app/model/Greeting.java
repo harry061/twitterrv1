6eec22c41091c46d6dc37104a76f4eb586d3edbe
harry061 <harisantoshtenti@gmail.com>
2018-08-09 01:40:52 +0530

    remove setter methods in model

--- TwitterrV1/src/main/java/com/twiterr/app/model/Greeting.java
+++ TwitterrV1/src/main/java/com/twiterr/app/model/Greeting.java
@@ -15,16 +15,8 @@ public class Greeting {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getContent() {
 		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
 	}
 
 }
