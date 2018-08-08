089a50a9710db127fcc12cb7a1137bd6ba0b2887
harry061 <harisantoshtenti@gmail.com>
2018-08-09 02:08:00 +0530

    JUnit commit

--- TwitterrV1/src/test/java/com/twiterr/app/TwitterrV1ApplicationTests.java
+++ TwitterrV1/src/test/java/com/twiterr/app/TwitterrV1ApplicationTests.java
@@ -1,16 +1,29 @@
 package com.twiterr.app;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class TwitterrV1ApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/greeting").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().json("{\"content\":\"Hello world\"}"));
 	}
 
 }
