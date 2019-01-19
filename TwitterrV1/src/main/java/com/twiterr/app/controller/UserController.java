2b4326d08647b713157d6da2890e3b03eec944ab
harry061 <harisantoshtenti@gmail.com>
2019-01-19 16:46:33 +0530

    Aded Local app Properties,Fetch User by ID

--- TwitterrV1/src/main/java/com/twiterr/app/controller/UserController.java
+++ TwitterrV1/src/main/java/com/twiterr/app/controller/UserController.java
@@ -2,6 +2,7 @@ package com.twiterr.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +15,17 @@ public class UserController {
 	@Autowired
 	private UserRepository userrepository;
 
+	
 	@RequestMapping("/getuser")
-	public User fetchUser() {
-		User u = new User();
-		u.setName("harry");
-		u = userrepository.save(u);
-		return userrepository.findById(u.getId()).get();
+	public User fetchUserbyID(@RequestParam(value = "Id", defaultValue = "1") Integer idVal) {
+		
+		return userrepository.findById(idVal).get();
 	}
 	
 	@RequestMapping("/getallusers")
 	public Iterable<User> getAllUsers()
 	{
+		
 		return userrepository.findAll();
 	}
 }
